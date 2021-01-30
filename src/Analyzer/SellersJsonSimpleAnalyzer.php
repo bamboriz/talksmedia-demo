6f3c365f0e1e15aad6f0416f05d3f0d034897ed6
Boris B <bamboriz1@gmail.com>
2021-01-30 13:59:04 +0100

    Feat: Analyzer service

--- src/Analyzer/SellersJsonSimpleAnalyzer.php
+++ src/Analyzer/SellersJsonSimpleAnalyzer.php
@@ -4,16 +4,48 @@ declare(strict_types=1);
 
 namespace App\Analyzer;
 
+use App\Entity\Seller;
+use Doctrine\ORM\EntityManagerInterface;
+use JsonSchema\Validator;
+
 /**
  * SellersJsonSimpleAnalyzer.
  */
 class SellersJsonSimpleAnalyzer implements SellersJsonAnalyzerInterface
 {
+    private array $sellers = [];
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function analyze(array $associativeSellersJson): void
     {
-        // TODO: Implement analyze() method.
+        $jsonSellersJson = (object) $associativeSellersJson;
+        $validator = new Validator();
+        $validator->validate($jsonSellersJson, (object) ['$ref' => 'file://'.realpath(__DIR__.'/config.json')]);
+
+        if ($validator->isValid()) {
+            foreach ($associativeSellersJson['sellers'] as $seller) {
+                $newSeller = new Seller();
+                $newSeller->setSellerId((int) $seller['seller_id']);
+                $newSeller->setType($seller['seller_type']);
+                $newSeller->setIsConfidential($seller['is_confidential']);
+
+                $this->entityManager->persist($newSeller);
+            }
+
+            $this->entityManager->flush();
+            $this->sellers = $associativeSellersJson['sellers'];
+        }
+    }
+
+    public function getSellers()
+    {
+        return $this->sellers;
     }
 }
