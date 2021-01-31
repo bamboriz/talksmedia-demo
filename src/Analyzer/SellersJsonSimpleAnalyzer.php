3e9a0136580d354eac04b522dd656af4273fd16f
Boris B <bamboriz1@gmail.com>
2021-01-31 21:19:27 +0100

    Fix: complete seller save

--- src/Analyzer/SellersJsonSimpleAnalyzer.php
+++ src/Analyzer/SellersJsonSimpleAnalyzer.php
@@ -27,20 +27,31 @@ class SellersJsonSimpleAnalyzer implements SellersJsonAnalyzerInterface
     {
         $jsonSellersJson = (object) $associativeSellersJson;
         $validator = new Validator();
-        $validator->validate($jsonSellersJson, (object) ['$ref' => 'file://'.realpath(__DIR__.'/config.json')]);
+        $validator->validate($jsonSellersJson, (object) ['$ref' => 'file://'.realpath(__DIR__.'/schema.json')]);
 
         if ($validator->isValid()) {
             foreach ($associativeSellersJson['sellers'] as $seller) {
                 $newSeller = new Seller();
                 $newSeller->setSellerId((int) $seller['seller_id']);
                 $newSeller->setType($seller['seller_type']);
-                $newSeller->setIsConfidential($seller['is_confidential']);
+                $newSeller->setIsConfidential($seller['is_confidential'] ?? 0);
+                $newSeller->setIsPassthrough($seller['is_passthrough'] ?? 0);
+                $newSeller->setName($seller['name'] ?? '');
+                $newSeller->setDomain($seller['domain'] ?? '');
+                $newSeller->setComment($seller['comment'] ?? '');
 
                 $this->entityManager->persist($newSeller);
             }
 
             $this->entityManager->flush();
             $this->sellers = $associativeSellersJson['sellers'];
+        }
+        else {
+            echo "Violation(s):\n";
+            foreach ($validator->getErrors() as $error) {
+                printf("[%s] %s\n", $error['property'], $error['message']);
+            }
+            throw new \Exception('sellers.json not valid.');
         }
     }
 
