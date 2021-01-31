5e49eb2d0ab4524695dbe049e987f2486d06e3eb
Boris B <bamboriz1@gmail.com>
2021-01-31 21:20:38 +0100

    Test: functional tests

--- tests/Analyzer/SellersJsonSimpleAnalyzerTest.php
+++ tests/Analyzer/SellersJsonSimpleAnalyzerTest.php
@@ -4,15 +4,22 @@ declare(strict_types=1);
 
 namespace App\Tests\Analyzer;
 
+use App\Entity\Seller;
+use App\Analyzer\SellersJsonSimpleAnalyzer;
+
 use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
 
 /**
  * SellersJsonSimpleAnalyzerTest.
  *
  * @coversDefaultClass \App\Analyzer\SellersJsonSimpleAnalyzer
  */
-class SellersJsonSimpleAnalyzerTest extends TestCase
+class SellersJsonSimpleAnalyzerTest extends KernelTestCase
 {
+    use RefreshDatabaseTrait;
+
     /**
      * Test that an exception is thrown when a not valid version is stored in the provided sellers.json.
      *
@@ -20,7 +27,13 @@ class SellersJsonSimpleAnalyzerTest extends TestCase
      */
     public function testAnalyzeWhenNotValidVersion(): void
     {
+        $this->expectExceptionMessage('sellers.json not valid.');
 
+        self::bootKernel();
+        $analyzer = self::$container->get(SellersJsonSimpleAnalyzer::class);
+    
+        $data = json_decode(file_get_contents(__DIR__.'/invalid_version_sellers.json'), true);
+        $analyzer->analyze($data);
     }
 
     /**
@@ -30,7 +43,13 @@ class SellersJsonSimpleAnalyzerTest extends TestCase
      */
     public function testAnalyzeWhenSellerHasNotValidType(): void
     {
+        $this->expectExceptionMessage('sellers.json not valid.');
 
+        self::bootKernel();
+        $analyzer = self::$container->get(SellersJsonSimpleAnalyzer::class);
+    
+        $data = json_decode(file_get_contents(__DIR__.'/invalid_sellers.json'), true);
+        $analyzer->analyze($data);
     }
 
     /**
@@ -40,6 +59,15 @@ class SellersJsonSimpleAnalyzerTest extends TestCase
      */
     public function testAnalyze(): void
     {
+        self::bootKernel();
+        $em = self::$container->get('doctrine')->getManager();
+        $analyzer = self::$container->get(SellersJsonSimpleAnalyzer::class);
 
+        $data = json_decode(file_get_contents(__DIR__.'/valid_sellers.json'), true);
+        $analyzer->analyze($data);
+        $sellers = $em->getRepository(Seller::class)->findAll();
+
+        // Assert that number of sellers in json equals number of sellers put into the db
+        $this->assertEquals(count($data["sellers"]), count($sellers));
     }
 }
